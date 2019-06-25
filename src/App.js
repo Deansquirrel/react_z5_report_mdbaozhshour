cd3ee307028d372a549250113fbf183944ce7f2a
yuansong <yuansong6@163.com>
2019-06-25 18:30:00 +0800

    201906251829

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {LocaleProvider} from 'antd';
 import {message} from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
 
 import { LoginForm } from "./Login/frmLogin"
-import { CReport } from "./Report/frmMdBaoZhShouR"
+// import { CReport } from "./Report/frmMdBaoZhShouR"
 
 import "antd/dist/antd.css";
 import "./App.css"
@@ -26,9 +27,10 @@ class Container extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            showPage:0,//0-init(null),1-login,-2-report
-            isLoggingIn:false,
+            showPage:0,//0-init(null),1-login,-2-pageLoader
             svrAddress:"",
+            isLoggingIn:false,
+            loginTime:new Date(),
             token:"",
         }
     }
@@ -36,7 +38,7 @@ class Container extends React.Component {
     componentDidMount() {
         this.refreshConfig();
         this.setState({
-            showPage:1,
+            showPage:2,
         })
     }
 
@@ -92,6 +94,7 @@ class Container extends React.Component {
                 if(data["errcode"]===0){
                     this.setState({
                         token:data["token"],
+                        loginTime:new Date(),
                         showPage:2,
                     });
                 } else {
@@ -121,11 +124,101 @@ class Container extends React.Component {
             case 2:
                 return (
                     <div>
-                        <CReport />
+                        <PageLoader />
                     </div>
                 );
             default:
                 return (<div>&nbsp;</div>);
         }
+    }
+}
+
+
+const { Header, Sider, Content } = Layout;
+const { SubMenu } = Menu;
+
+class PageLoader extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            showPage:0,//0-init(null),1-login,-2-report
+            collapsed: false,
+        }
+    }
+
+    toggle = () => {
+        this.setState({
+            collapsed:!this.state.collapsed,
+        })
+    };
+
+    render() {
+
+        const testData = [
+            {key:"mdReport",icon:"table",title:"门店报表",child:[
+                    {key:"mdBaoZhShouR",title:"门店报账收入日报"},
+                    {key:"mdBaoZhShouR2",title:"门店报账收入日报2"}
+                ]},
+        ];
+
+        const MenuList = ({mData,defaultKey,defaultOpenKey}) => {
+            return (
+                <Menu theme="dark"  mode="inline" defaultSelectedKeys={[defaultKey]} defaultOpenKeys={[defaultOpenKey]}>
+                    {mData.map((item)=>{
+                        if (item.child.length > 0){
+                            return (
+                                <SubMenu key={item.key} title ={<span><Icon type={item.icon} /><span>{item.title}</span></span>}>
+                                    {item.child.map((subItem)=>{
+                                        return (
+                                            <Menu.Item key={subItem.key}>
+                                                <span>{subItem.title}</span>
+                                            </Menu.Item>
+                                        )
+                                    })}
+                                </SubMenu>
+                            )
+                        } else {
+                            return (
+                                <Menu.Item key={item.key}>
+                                    <Icon type={item.icon} />
+                                    <span>{item.title}</span>
+                                </Menu.Item>
+                            )
+                        }
+
+                    })}
+                </Menu>
+            )
+        };
+
+        return (
+            <Layout>
+                <Sider width={256} style={{minHeight:'100vh'}} trigger={null} collapsible collapsed={this.state.collapsed}>
+                    <div className="logo" />
+                    <MenuList mData={testData} defaultKey = {"mdBaoZhShouR"} defaultOpenKey={"mdReport"} />
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#fff', padding: 0 , width: '100%' }}>
+                        <Icon
+                            className="trigger"
+                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                            onClick={this.toggle}
+                        />
+                    </Header>
+                    <Content
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            background: '#fff',
+                        }}
+                    >
+                        Content<br />
+                        Content<br />
+                        Content<br />
+                        Content<br />
+                    </Content>
+                </Layout>
+            </Layout>
+        );
     }
 }
