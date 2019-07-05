f6997b02b6ee51a4325bc0ec6073538f911a0a5e
yuansong <yuansong6@163.com>
2019-07-05 21:34:24 +0800

    201907052134

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ import "antd/dist/antd.css";
 import "./App.css"
 import $ from 'jquery'
 
-// const preVersionInfo = "1.0.1 Build20190703";
+// const preVersionInfo = "1.0.2 Build20190705";
 // const testVersionInfo = "0.0.0 Build20190101";
-const versionInfo = "1.0.2 Build20190705";
+const versionInfo = "0.0.0 Build20190101";
 
 moment.locale('zh-cn');
 message.config({
